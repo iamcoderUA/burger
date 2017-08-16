8b0f34e2c8d844b3681d9bb0ca962e4feaa5fa82
stanislavMikniev <stanislav.mikhniev@gmail.com>
2017-08-16 21:14:05 +0300

    add modal popups draft

---
+++ production/js/popup.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('[data-fancybox]').fancybox();
+	$('.review__close').on('click', function(e){
+		e.preventDefault;
+		$.fancybox.close();
+	});
+	$('.form-popup__close').on('click', function(e){
+		e.preventDefault;
+		$.fancybox.close();
+	});
+});
