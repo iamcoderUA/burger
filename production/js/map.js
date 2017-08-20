ca44eb808060b2a94d7be4b5eed988262a7075b6
stanislavMikniev <stanislav.mikhniev@gmail.com>
2017-08-21 00:05:18 +0300

    add slider, one page scroll, google map API vol.2

--- production/js/map.js
+++ production/js/map.js
@@ -1,31 +1,29 @@
-	
-	var map;
-	function initMap() {
-		var icons = '../img/icons/map-marker.svg',
-		map = new google.maps.Map(document.getElementById('map'), 
+window.initMap = function() {
+	var icons = '../img/icons/map-marker.svg',
+	map = new google.maps.Map(document.getElementById('map'), 
+	{
+		center: {lat: 59.9276608, lng: 30.3108849},
+		zoom: 12,
+		disableDefaultUI: true
+	});
+
+	map.setOptions({draggable: true, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: false});
+
+	var markers = 
+	[
+	{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.3108849}},
+	{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.2108849}},
+	{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.4108849}},
+	{title: 'ул. ШейхЗаид, стр.5', location: {lat: 60.9276608, lng: 30.3108849}}
+	]
+
+	markers.forEach(function(feature) {
+		var marker = new google.maps.Marker(
 		{
-			center: {lat: 59.9276608, lng: 30.3108849},
-			zoom: 12,
-			disableDefaultUI: true
+			position: feature.location,
+			title: feature.title,
+			icon: icons,
+			map: map
 		});
-
-		map.setOptions({draggable: true, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: false});
-
-		var markers = 
-		[
-		{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.3108849}},
-		{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.2108849}},
-		{title: 'ул. ШейхЗаид, стр.5', location: {lat: 59.9276608, lng: 30.4108849}},
-		{title: 'ул. ШейхЗаид, стр.5', location: {lat: 60.9276608, lng: 30.3108849}}
-		]
-
-		markers.forEach(function(feature) {
-			var marker = new google.maps.Marker(
-			{
-				position: feature.location,
-				title: feature.title,
-				icon: icons,
-				map: map
-			});
-		});
-	}
+	});
+}
