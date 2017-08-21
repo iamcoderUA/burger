d16ca4d22f7183af4154931dce71ef42879783d9
stanislavMikniev <stanislav.mikhniev@gmail.com>
2017-08-22 02:00:11 +0300

    changed structure order-form.js

--- production/js/order-form.js
+++ production/js/order-form.js
@@ -3,7 +3,8 @@ var submitForm = function (ev) {
 
     var form = $(ev.target),
     inp = $('.order-form__input'),
-    trigger = 0;
+    trigger = 0,
+    request = ajaxForm(form);
 
     inp.each( function(e){
         var $this = $(this);
@@ -14,34 +15,24 @@ var submitForm = function (ev) {
                 $this.css("border", "2px solid red");
                 return trigger = 1;
             }; 
-        }
-    })
+        };
+    });
 
-    var ajaxForm = function (form) {
-
-        var url = form.attr('action'),
-        data = form.serialize();
-
-        return $.ajax({
-            type: 'POST',
-            url: url,
-            data: data,
-            dataType: 'JSON'
-        }).done(function(msg) {
+    if (trigger === 0) {
+        request.done(function(msg) {
             var mes = msg.mes,
             status = msg.status;
             if (status === 'OK') {
                 alert(mes);
-
-                // $('[data-fancyform-success]').fancybox();
+                // $('[data-fancyform-success]').fancybox(); // срабатывает только со второго раза!!!
                 // $('.form-popup__close').on('click', function(e){
                 //     e.preventDefault();
                 //     $.fancybox.close();
+                //     $('#form').get(0).reset();
                 // });
                 $('#form').get(0).reset();
             } else{
                 alert('<p class="error">' + mes + '</p>');
-
                 // $('[data-fancyform-error]').fancybox();
                 // $('.form-popup__close').on('click', function(e){
                 //     e.preventDefault();
@@ -50,19 +41,22 @@ var submitForm = function (ev) {
             }
         }).fail(function(jqXHR, textStatus) {
             alert("Request failed: " + textStatus + ' - будешь голодным');
-
-            // $('[data-fancyform-error]').fancybox();
-            // $('.form-popup__close').on('click', function(e){
-            //     e.preventDefault();
-            //     $.fancybox.close();
-            // });
         });
-    }
-
-    if (trigger === 0) {
-        ajaxForm(form);
     } else {
         alert('Хочешь кушать? Заполни все поля, пожалуйста!');
-    }
-}
+    }   
+};
+
+var ajaxForm = function (form) {
+    var url = form.attr('action'),
+    data = form.serialize();
+
+    return $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        dataType: 'JSON'
+    });
+};
+
 $('#form').on('submit', submitForm);
