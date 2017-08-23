6182df9fe615ed1e283033bf2e3fcb10ecc0cd47
stanislavMikniev <stanislav.mikhniev@gmail.com>
2017-08-23 11:24:32 +0300

    update acco_h.js and fixed fsmenu.js

--- production/js/acco_h.js
+++ production/js/acco_h.js
@@ -6,32 +6,49 @@ $(function() {
 		var elem = $(e.target),
 		item = elem.closest('.menu-list__item'),
 		descrip = item.find('.menu-list__description'),
-		reqHeight = item.find('.menu-list__text').outerWidth(),
-		textAccoH = item.find('.menu-list__text'),
 		items = item.siblings(),
-		otherContent = items.find('.menu-list__description');
+		otherContent = items.find('.menu-list__description'),
+		animWidth,
+		reqWidth,
+		windowWidth,
+		itemsLength;
+		
 
+		if (!item.hasClass('active')) {
+			items.removeClass('active');
+			item.addClass('active')
 
-		if(window.matchMedia('(min-width: 300px)').matches)
-		{
-			if (!item.hasClass('active')) {
-				items.removeClass('active');
-				item.addClass('active')
-
-				// otherContent.css({
-					// 'width': 0,
-				// });
-				// descrip.css({
-					// 'width' : '100%'
-				// });
-				// textAccoH.fadeIn(1000); // не работает!
-			} else {
-				item.removeClass('active')
-				// descrip.css({
-				// 	'width' : 0
-				// });
+			otherContent.css('width', 0);
+			
+			function result() {
+				windowWidth = $(window).width(),
+				itemsLength = $('.menu-list__item').width() * $('.menu-list__item').length;
+				return reqWidth = windowWidth - itemsLength;
 			}
-			return false;
-		};
+			console.log(result());
+
+			if ($(window).width() <= 768) {
+				animWidth = reqWidth;
+			} else {
+				animWidth = '50vw'
+			};
+			// descrip.removeAttr('style');
+
+			descrip.css({
+				'width' :  animWidth
+			});
+		} else {
+			item.removeClass('active')
+			descrip.css({
+				'width' :  0
+			});
+		}
+		return false;
 	});
 });
+
+
+
+
+
+
--- production/js/fullscreen.js
+++ production/js/fullscreen.js
@@ -4,23 +4,18 @@ $(document).ready(function() {
 		e.preventDefault();
 
 		var fsMenuActive = $(e.target).closest('.maincontent').next('.hamburger-menu').addClass('active'),
-				navOpen =	fsMenuActive.find('.nav').addClass('active');
-				navOpen.slideDown(600);
+		navOpen =	fsMenuActive.find('.nav').addClass('active');
+		navOpen.slideDown(600);
 	});
 
 	$('.hamburger-menu__close').on('click', function(e) {
 		e.preventDefault();
-		
-		var fsMenuClose = $(e.target).closest('.hamburger-menu').removeClass('active');
-				navClose = fsMenuClose.find('.nav').removeClass('active');
-				navClose.slideUp(100);
+		$('.hamburger-menu').removeClass('active').find('.nav').removeClass('active').slideUp(100);
 	});
 
-	$('.nav__link').on('click', function(e) {
-		// e.preventDefault();
-
-		var fsMenuClose = $(e.target).closest('.hamburger-menu').removeClass('active');
-				navClose = fsMenuClose.find('.nav').removeClass('active');
-				navClose.slideUp(100);
+	$('.nav__link').on('click touchstart', function(e) {
+		$('.hamburger-menu').removeClass('active').find('.nav').removeClass('active').slideUp(100);
 	});
 });
+
+
